a919952c3e9cf7f85f59b0249a9c940aa7db411a
Lucas M.R. <lmr2106@hotmail.com>
2025-05-10 11:26:03 -0300

    Time: 39 ms (8.21%), Space: 21.7 MB (36.64%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        positions = defaultdict(list)
+
+
+        for s in strs:
+            word_count = defaultdict(int)
+            for c in s:
+                word_count[c] += 1
+            key = ""
+            for k in sorted(word_count.keys()):
+                key = f"{key}{k}{word_count[k]}"
+            positions[key].append(s)
+
+        ans = []
+        print(positions)
+        for val in positions.values():
+            ans.append(val)
+        return ans
+        
+
+
+
